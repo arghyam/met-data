71f37ca57d64c30c14ec7155127ed0f2c145abf8
Kushagra Gupta <kushagragupta625@gmail.com>
2024-06-30 03:46:51 +0530

    set up the frontend directory

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 >
+          Welcome to Met Data Visualization by the India Water Portal
+      </h1>
+    </main>
+  );
+}
