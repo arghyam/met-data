558e6b14d909f188b9e6dcf35f56e2c9bb4d141a
Kushdapush <129064700+Kushdapush@users.noreply.github.com>
2024-07-10 18:40:11 +0530

    [1] - feat(frontend) : Created the Home page (#7)
    * set up the frontend directory
    
    * creates the Home page

---
+++ frontend/src/components/InfoCard/index.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+type InfoCardProps = {
+  title: string;
+  description: string;
+  image: any;
+  link: string;
+  altText: string;
+};
+
+export default function InfoCard({
+  title,
+  description,
+  image,
+  link,
+  altText,
+}: InfoCardProps) {
+  return (
+    <div className="max-w-sm bg-white border border-gray-400 rounded-lg shadow transition-transform transform hover:scale-105 dark:bg-gray-800 dark:border-gray-700">
+      <div className="lg:h-80 bg-white">
+        <Link href={link}>
+          <Image
+            src={image}
+            alt={altText}
+            className="rounded-t-lg object-cover w-full h-full"
+          />
+        </Link>
+      </div>
+      <div className="p-5">
+        <Link href={link}>
+          <div>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {title}
+            </h5>
+          </div>
+        </Link>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {description}
+        </p>
+        <Link href={link}>
+          <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Go here
+            <svg
+              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,49 +1,117 @@
 "use client";
+import { useState, useEffect, use } from "react";
 
-import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import classes from "./index.module.css";
 import Link from "next/link";
 import Image from "next/image";
-import { AnimatePresence, motion } from "framer-motion";
+
+import IWPlogo from "@/../public/IWPlogo.svg";
 
 const navigationItems = [
-    { label: "Home", href: "/" },
-    { label: "Visualizations", href: "/Visualizations" },
-    { label: "Statistics", href: "/Statistics" },
-    { label: "Return", href: "https://www.indiawaterportal.org/articles/background-meteorological-datasets" }
-  ];
+  { label: "Home", href: "/" },
+  { label: "Visualizations", href: "/visualizations" },
+  { label: "Statistics", href: "/statistics" },
+  {
+    label: "Return",
+    href: "https://www.indiawaterportal.org/articles/background-meteorological-datasets",
+  },
+];
 
 type NavItemProps = {
-    label: string;
-    href: string;
+  label: string;
+  href: string;
 };
-  
-const NavItem : React.FC<NavItemProps> = ({ label, href}) => {
-    const pathname = usePathname();
-  
-    return (
+
+const NavItem: React.FC<NavItemProps> = ({ label, href }) => {
+  const pathname = usePathname();
+
+  return (
+    <div
+      className={`transition-all ease-in-out focus-visible:outline-2 ${
+        pathname === href ? "" : "hover:bg-grey-100"
+      }`}
+    >
+      <Link href={href}>
+        <p className="text-lg md:text-base lg:text-lg text-center">{label}</p>
+      </Link>
+      <div className="mx-8"></div>
+    </div>
+  );
+};
+
+export default function Navbar() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isMenuOpen]);
+
+  return (
+    <>
+      <nav className="absolute sm:z-[2] w-full flex items-center xl:justify-center px-4 pt-8">
+        <Image
+          src={IWPlogo}
+          alt="India-Water-Portal-Logo"
+          className="absolute w-[12rem]  h-[4rem] pl-[2rem] left-0"
+          priority
+        />
+        <div className="hidden h-10 w-3/5 xl:flex xl:justify-between xl:items-center px-3 py-8 rounded-full border-1 border-solid border-emrald-800 bg-slate-200 bg-opacity-60 backdrop-blur-xl space-x-2">
+      {navigationItems.map((item) => (
         <div
-          className={`group relative text-[#9A9A9A] font-[600] px-6 py-2 
-          rounded-full transition-all ease-in-out focus-visible:outline-2 ${
-            pathname === href ? "" : "hover:bg-[#FFFFFF10]"
-          }`}
+          key={item.href}
+          className={`w-32 font-[600] py-2 flex justify-center rounded-full transition-all ease-in-out focus-visible:outline-2`}
         >
-          <AnimatePresence initial={false} mode="wait">
-            {pathname === href && (
-              <motion.span
-                layoutId="bubble"
-                className="absolute inset-0 z-10 bg-[#FFFFFF1A] mix-blend-difference"
-                style={{ borderRadius: 9999 }}
-                transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
-                key={label}
-              />
-            )}
-          </AnimatePresence>
-          <Link href={href}>
-            <p className="text-lg md:text-base lg:text-lg text-center">{label}</p>
-          </Link>
-          <div className="mx-2"></div>
+          <div className="text-[#159AB2] hover:text-[#036876] hover:bg-gray-300 rounded-full py-2 px-4">
+            <NavItem label={item.label} href={item.href} />
+          </div>
         </div>
-    );
-};
+      ))}
+    </div>
+
+        <div className="flex items-center justify-end w-full xl:hidden">
+          <button
+            id="menu-btn"
+            aria-label="Toggle Menu"
+            type="button"
+            className={`z-40 hamburger xl:hidden focus:outline-none ${
+              isMenuOpen ? classes.open : ""
+            } ${classes.hamburger}`}
+            onClick={() => setMenuOpen(!isMenuOpen)}
+          >
+            <span className={classes.hamburgerTop}></span>
+            <span className={classes.hamburgerMiddle}></span>
+            <span className={classes.hamburgerBottom}></span>
+          </button>
+        </div>
+      </nav>
+      <div
+        id="menu"
+        className={`absolute z-[1] top-0 bottom-0 left-0 ${
+          isMenuOpen ? "block" : "hidden"
+        } w-full min-h-screen py-1 pt-40 px-8 backdrop-blur-lg`}
+      >
+        <div
+          className="flex flex-col self-end space-y-8 text-lg text-[#159AB2] hover:text-[#036876] 
+        font-medium uppercase p-8 border-1 border-[#222] rounded-[2rem] 
+        bg-sky-200 bg-opacity-80"
+        >
+          {navigationItems.map(({ label, href }) => (
+            <Link
+              href={href}
+              key={label}
+              className="hover:text-[#036876]"
+              onClick={() => setMenuOpen(false)}
+            >
+              {label}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
