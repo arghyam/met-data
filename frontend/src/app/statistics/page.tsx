f5f8bb9e15615016f2cbfe8f0df48156a8579032
Kushagra Gupta <kushagragupta625@gmail.com>
2024-07-14 04:29:55 +0530

    Adds the filter data

--- frontend/src/app/statistics/page.tsx
+++ frontend/src/app/statistics/page.tsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import Navbar from "@/components/Navbar"
+
+
 export default function Statistics() {
+
+    const[state, setState] = useState();
+    const[district, setDistrict] = useState();
+    const[parameter, setParameter] = useState();
+    const[startingYear, setStartingYear] = useState();
+    const[endingYear, setEndingYear] = useState();
+    const[infoType, setInfoType] = useState();
+
     return (
-        <h1>Statistics</h1>
+        <>
+        <Navbar />
+        <div className="flex flex-col items-center justify-between p-24">
+            <h1 className="mt-24 text-3xl font-bold">Mean Values</h1>
+        </div>
+        </>
     )
 };
