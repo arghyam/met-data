f18548bd0929d5b85ceacc67122c0886c9d6a3a3
Kushagra Gupta <kushagragupta625@gmail.com>
2025-01-02 00:11:10 +0530

    changes the InfoCard component and removes the navbar

--- frontend/src/components/InfoCard/index.tsx
+++ frontend/src/components/InfoCard/index.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
 
@@ -19,47 +17,27 @@ export default function InfoCard({
   altText,
 }: InfoCardProps) {
   return (
-    <div className="max-w-sm bg-white  rounded-lg shadow transition-transform transform hover:scale-105 dark:bg-gray-800 dark:border-gray-700">
-      <div className="lg:h-80 bg-white">
-        <Link href={link}>
+    <div className="max-w-[18rem] bg-white rounded-lg shadow transition-transform transform hover:scale-105 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-between">
+      <Link href={link} legacyBehavior>
+        <a className="block h-64 bg-white rounded-t-lg overflow-hidden">
           <Image
             src={image}
             alt={altText}
-            className="rounded-t-lg object-contain w-full h-full"
+            className="object-contain w-full h-full"
           />
-        </Link>
-      </div>
-      <div className="p-5">
-        <Link href={link}>
-          <div>
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {title}
-            </h5>
-          </div>
-        </Link>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        </a>
+      </Link>
+      <div className="p-5 bg-[#D9D9D9] flex flex-col flex-grow text-center">
+        <p className="mb-3 font-normal text-black">
           {description}
         </p>
-        <Link href={link}>
-          <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Go here
-            <svg
-              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </div>
-        </Link>
+        <div className="flex justify-center mt-auto">
+          <Link href={link} legacyBehavior>
+            <a className="inline-flex items-center justify-center w-1/2 px-5 py-3 text-lg font-medium text-center text-white bg-[#0097B2] rounded-full hover:bg-[#008FA3] focus:ring-4 focus:outline-none focus:ring-[#008FA3] dark:bg-[#0097B2] dark:hover:bg-[#008FA3] dark:focus:ring-[#008FA3]">
+              Start
+            </a>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/InfoSection/index.tsx
+++ frontend/src/components/InfoSection/index.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import statistics from "@/../public/statistics.webp";
 import visualizations from "@/../public/visualizations.webp";
 import InfoCard from "../InfoCard";
@@ -7,14 +5,14 @@ import InfoCard from "../InfoCard";
 const cardItems = [
   {
     title: "Statistics",
-    description: "Statistics: We'll compute the annual and monthly averages and totals for various meteorological parameters. This will give us a broad overview of the data.",
+    description: "Simplify data insights with computed yearly and monthly stats",
     image: statistics,
     link: "/statistics",
     altText: "statistics",
   },
   {
     title: "Visualizations",
-    description: "Trend Plots: These are used to visualize the change in a parameter over time.They are essentially line graphs that can show trends or patterns over a specified period.",
+    description: "Visualize trends over time with line graphs that reveal patterns at a glance",
     image: visualizations,
     link: "/visualizations",
     altText: "visualizations",
@@ -24,16 +22,13 @@ const cardItems = [
 export default function InfoSection() {
   return (
     <div className="mb-10">
-      <div className="justify-center items-center m-4 mt-10 lg:mt-8 sm:px-16 xl:px-48">
+      <div className="m-4 mt-10 lg:mt-8 sm:px-16 xl:px-48 flex flex-col items-center">
         <h1 className="mb-3 text-center font-extrabold leading-7 tracking-normal text-gray-800 md:text-2xl lg:text-[40px]">
-          Insights that we provide
+          Explore
         </h1>
-        <div className="grid lg:grid-cols-2 gap-40 mt-12 text-left font-normal text-gray-600 lg:text-xl">
+        <div className="grid lg:grid-cols-2 lg:gap-60 gap-10 mt-24 text-left font-normal text-gray-600 lg:text-xl">
           {cardItems.map((card, index) => (
-            <InfoCard
-              key={index}
-              {...card}
-            />
+            <InfoCard key={index} {...card} />
           ))}
         </div>
       </div>
